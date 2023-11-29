83b8d225f0cae55286261cbddd44957de3640ed9
Adrian Sumagang <asumagang@dnamicro.com>
2023-11-30 07:26:03 +0800

    EN: Setup environment.

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -34,6 +34,13 @@ module.exports = {
   ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js", "**/*.css"],
   // add rules configurations here
   rules: {
-    "import/no-default-export": "off",
+    "import/prefer-default-export": off,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
   },
 };
